1e2c0a7a97d6410533e436fbbe24c4f08cba1e10
zjx <zjxcyr@qq.com>
2022-07-22 13:42:34 +0800

    initical project

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from . import models
-
-admin.site.site_header=u"这是你能看的地方吗"
-admin.site.site_title = u"个人管理界面-非礼勿视"
-
-admin.site.register(models.User)
