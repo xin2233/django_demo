1840b2024f3f81f6981ce6a745e051671ebc88de
zjx <zjxcyr@qq.com>
2023-02-10 11:22:27 +0800

    add 前端，订单处理

--- myobject/myadmin/models.py
+++ myobject/myadmin/models.py
@@ -98,3 +98,46 @@ class Product(models.Model):
 
     class Meta:
         db_table = "product"  # 更改表名
+
+
+# 订单模型
+class Orders(models.Model):
+    shop_id = models.IntegerField()   #店铺id号
+    member_id = models.IntegerField() #会员id
+    user_id = models.IntegerField()   #操作员id
+    money = models.FloatField()     #金额
+    status = models.IntegerField(default=1)   #订单状态:1过行中/2无效/3已完成
+    payment_status = models.IntegerField(default=1)   #支付状态:1未支付/2已支付/3已退款
+    create_at = models.DateTimeField(default=datetime.now)  #创建时间
+    update_at = models.DateTimeField(default=datetime.now)  #修改时间
+
+    class Meta:
+        db_table = "orders"  # 更改表名
+
+
+# 订单详情模型
+class OrderDetail(models.Model):
+    order_id = models.IntegerField()  #订单id
+    product_id = models.IntegerField()  #菜品id
+    product_name = models.CharField(max_length=50) #菜品名称
+    price = models.FloatField()     #单价
+    quantity = models.IntegerField()  #数量
+    status = models.IntegerField(default=1) #状态:1正常/9删除
+
+    class Meta:
+        db_table = "order_detail"  # 更改表名
+
+
+# 支付信息模型
+class Payment(models.Model):
+    order_id = models.IntegerField()  #订单id号
+    member_id = models.IntegerField() #会员id
+    money = models.FloatField()     #支付金额
+    type = models.IntegerField()      #付款方式：1会员付款/2收银收款
+    bank = models.IntegerField(default=1) #收款银行渠道:1微信/2余额/3现金/4支付宝
+    status = models.IntegerField(default=1) #支付状态:1未支付/2已支付/3已退款
+    create_at = models.DateTimeField(default=datetime.now)  #创建时间
+    update_at = models.DateTimeField(default=datetime.now)  #修改时间
+
+    class Meta:
+        db_table = "payment"  # 更改表名
