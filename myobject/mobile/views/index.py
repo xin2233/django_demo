97177ecb258005a6848a13fe1f20d8eb22503b1f
zjx <zjxcyr@qq.com>
2023-03-10 15:10:34 +0800

    add 移动端点餐，会员下单功能

--- myobject/mobile/views/index.py
+++ myobject/mobile/views/index.py
@@ -3,6 +3,7 @@ import datetime
 
 from django.shortcuts import render, redirect, reverse
 from myadmin.models import Member, Shop, Category, Product
+from myadmin.models import Orders, OrderDetail, Payment
 
 
 # Create your views here.
@@ -10,7 +11,7 @@ def index(request):
     """移动端首页"""
     # 尝试从session中获取店铺信息
     shopinfo = request.session.get("shopinfo")
-    print("s",shopinfo)
+    print("s", shopinfo)
     # 判断是否没有选择店铺，跳转去选择
     if shopinfo is None:
         return redirect(reverse('mobile_shop'))
@@ -21,7 +22,7 @@ def index(request):
     for vo in clist:
         plist = Product.objects.filter(shop_id=shopinfo['id'], category_id=vo.id, status=1)
         productlist[vo.id] = plist
-        print('p',plist)
+        print('p', plist)
     context = {"shopinfo": shopinfo, "categorylist": clist, "productlist": productlist.items(), "cid": clist[0]}
     return render(request, "mobile/index.html", context)
 
@@ -83,5 +84,60 @@ def doregister(request):
 
 
 def addOrders(request):
-    """ 加载准备下订单页，由会员确认 """
+    ''' 移动端下单表单页'''
+    # 尝试从session中获取名字为cartlist的购物车信息，若没有返回{}
+    cartlist = request.session.get('cartlist', {})
+    total_money = 0  # 初始化一个总金额
+    # 遍历购物车中的菜品并累加总金额
+    for vo in cartlist.values():
+        total_money += vo['num'] * vo['price']
+    request.session['total_money'] = total_money  # 放进session
     return render(request, "mobile/addOrders.html")
+
+
+def doAddOrders(request):
+    ''' 执行移动端下单操作'''
+    try:
+        # 执行订单信息的添加
+        od = Orders()
+        od.shop_id = request.session['shopinfo']['id']
+        od.member_id = request.session['mobileuser']['id']
+        od.user_id = 0
+        od.money = request.session['total_money']
+        od.status = 1  # 订单状态:1过行中/2无效/3已完成
+        od.payment_status = 2  # 支付状态:1未支付/2已支付/3已退款
+        od.create_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        od.update_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        od.save()
+
+        # 执行支付信息添加
+        op = Payment()
+        op.order_id = od.id  # 订单id号
+        op.member_id = 0
+        op.type = 2
+        op.bank = request.GET.get("bank", 3)  # 收款银行渠道:1微信/2余额/3现金/4支付宝
+        op.money = request.session['total_money']
+        op.status = 2  # 支付状态:1未支付/2已支付/3已退款
+        op.create_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        op.update_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        op.save()
+
+        # 执行订单详情的添加
+        cartlist = request.session.get("cartlist", {})  # 获取购物车中的菜品信息
+        # 遍历购物车中的菜品并添加到订单详情中
+        for item in cartlist.values():
+            ov = OrderDetail()
+            ov.order_id = od.id  # 订单id
+            ov.product_id = item['id']  # 菜品id
+            ov.product_name = item['name']  # 菜品名称
+            ov.price = item['price']  # 单价
+            ov.quantity = item['num']  # 数量
+            ov.status = 1  # 状态:1正常/9删除
+            ov.save()
+
+        # 清空session购物车信息
+        del request.session["cartlist"]
+        del request.session['total_money']
+    except Exception as err:
+        print(err)
+    return render(request, "mobile/orderinfo.html", {"order": od})
